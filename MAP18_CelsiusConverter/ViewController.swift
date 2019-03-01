0b2ef2e590330a4747c0cffb54a6337b0b91db88
David Svensson <david.svensson@nackademin.se>
2019-03-01 11:02:55 +0100

    datasource

--- MAP18_CelsiusConverter/ViewController.swift
+++ MAP18_CelsiusConverter/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+      return 10
+    }
+    
 
-
+   
 }
 
